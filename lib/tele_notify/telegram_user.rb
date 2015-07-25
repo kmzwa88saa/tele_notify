d76d74744489cc1cf967c6783178f3da3159910f
ppati000 <ppati000@me.com>
2015-07-25 18:41:01 +0200

    Add TelegramUser Methods

--- lib/tele_notify/telegram_user.rb
+++ lib/tele_notify/telegram_user.rb
@@ -1,8 +1,36 @@
+require 'rest-client'
 
 module TeleNotify
   class TelegramUser < ::ActiveRecord::Base
 
     validates_presence_of :telegram_id
+
+    def self.configure_token(token)
+      if token =~ /^[0-9]+:[\w-]+$/
+        @token = token
+        @url = "https://api.telegram.org/bot" + token + "/"
+      else
+        raise "Invalid token."
+      end
+    end
+
+    def self.get_updates
+      response = RestClient.get(@url + "getUpdates")
+      if response["ok"]
+        puts response
+        puts response["result"]
+        updates = response["result"]
+        updates.each do |update|
+          self.class.create( { telegram_user_id: update["message"]["from"]["id"], first_name: update["message"]["from"]["first_name"] } )
+        end
+      end
+    end
+
+    def self.send_message
+      self.class.all.each do |user|
+        RestClient.post(@url + "sendMessage", chat_id: user.telegram_user_id, text: "Test")
+      end
+    end
 
   end
 
