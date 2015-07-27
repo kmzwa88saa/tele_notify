ef664ac62fa72ef07679aa594fafba375660ce3e
ppati000 <ppati000@me.com>
2015-07-27 20:43:09 +0200

    Reorganize send_message

--- lib/tele_notify/telegram_user.rb
+++ lib/tele_notify/telegram_user.rb
@@ -6,35 +6,44 @@ module TeleNotify
     validates_presence_of :telegram_id
     validates_uniqueness_of :telegram_id
 
-    @next_update_id = 0
+    @@next_update_id = 0
 
     def self.configure_token(token)
       if token =~ /^[0-9]+:[\w-]+$/ #hacker proof
-        @token = token
-        @url = "https://api.telegram.org/bot" + token + "/"
+        @@token = token
+        @@url = "https://api.telegram.org/bot" + token + "/"
       else
         raise "Invalid token."
       end
     end
 
     def self.get_updates
-      puts @next_update_id
-      response = JSON.parse(RestClient.post(@url + "getUpdates", { offset: @next_update_id }), { symbolize_names: true })
+      puts @@next_update_id
+      response = JSON.parse(RestClient.post(@@url + "getUpdates", { offset: @@next_update_id }), { symbolize_names: true })
       puts response
       if response[:ok]
         updates = response[:result]
         updates.each do |update|
           self.create( { telegram_id: update[:message][:from][:id], first_name: update[:message][:from][:first_name] } )
-          @next_update_id = update[:update_id] + 1
+          @@next_update_id = update[:update_id] + 1
         end
       end
     end
 
-    def self.send_message
+    def self.send_message_to_all(text)
+      success = true
       TeleNotify::TelegramUser.all.each do |user|
-        RestClient.post(@url + "sendMessage", chat_id: user.telegram_id, text: "Test")
+        success = false if !user.send_message(text)
       end
+      success
     end
+
+
+    def send_message(text)
+      response = JSON.parse(RestClient.post(@@url + "sendMessage", chat_id: self.telegram_id, text: text), { symbolize_names: true })
+      response[:ok]
+    end
+
 
   end
 
--- lib/tele_notify/version.rb
+++ lib/tele_notify/version.rb
@@ -1,3 +1,3 @@
 module TeleNotify
-  VERSION = "0.0.4.12"
+  VERSION = "0.0.4.13"
 end
