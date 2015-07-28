6afc18c057c8c78d2f6544bbd0b8d7dc476be89b
ppati000 <ppati000@me.com>
2015-07-28 14:06:08 +0200

    Remove app

--- test/TeleTest/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
-  #IMPORTANT! THESE TWO LINES MUST COME AFTER protect_from_forgery!
-  skip_before_filter :verify_authenticity_token, :only => :webhook
-  include TeleNotify::Controller
-end
