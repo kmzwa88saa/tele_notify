c2eac8aa55e49e7112e79c0c9e49ea4894aace45
ppati000 <ppati000@me.com>
2015-07-28 13:43:06 +0200

    Fix app and change default config

--- lib/generators/tele_notify/migration/templates/active_record/tele_notify.rb
+++ lib/generators/tele_notify/migration/templates/active_record/tele_notify.rb
@@ -2,14 +2,14 @@
 #For production, just use your URL (e.g. https://myapp.com)
 #You MUST NOT include a trailing slash and it MUST be https!
 #INVALID URLS: e.g. http://myapp.com or https://myapp.com/
-TeleNotify::TelegramUser.configure_home_url("https://telenotify.herokuapp.com")
+TeleNotify::TelegramUser.configure_home_url("YOUR PRODUCTION URL")
 
 #For development, download ngrok from https://ngrok.com/.
 #Extract it and run "./ngrok http 3000"
 #Then copy the URL you get from the console window.
 #Remember to use the HTTPS URL!
-TeleNotify::TelegramUser.configure_dev_url("https://42ebfa35.ngrok.io")
+TeleNotify::TelegramUser.configure_dev_url("YOUR NGROK DEVELOPMENT URL")
 
 #Set your Telegram Bot API token here
 #Don't have your token yet? Create your bot using https://telegram.me/botfather
-TeleNotify::TelegramUser.configure_token("YOUR TOKEN HERE")
+TeleNotify::TelegramUser.configure_token("YOUR TOKEN")
