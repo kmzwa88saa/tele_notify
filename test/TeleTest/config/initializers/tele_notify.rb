482f10abaacff5a991ce0312ec331768992e38d7
ppati000 <ppati000@me.com>
2015-07-28 13:44:24 +0200

    Fix app

--- test/TeleTest/config/initializers/tele_notify.rb
+++ test/TeleTest/config/initializers/tele_notify.rb
@@ -1,8 +1,3 @@
-#Set your Telegram Bot API token here
-#Don't have your token yet? Create your bot using https://telegram.me/botfather
-TeleNotify::TelegramUser.configure_home_url("https://42ebfa35.ngrok.io")
-TeleNotify::Te
-TeleNotify::TelegramUser.configure_token("66381462:AAG1q2brO6DcXnmmn70HwBMw9ReYhS-bF8Y")
 #Set your home URL, so Telegram callbacks work
 #For production, just use your URL (e.g. https://myapp.com)
 #You MUST NOT include a trailing slash and it MUST be https!
